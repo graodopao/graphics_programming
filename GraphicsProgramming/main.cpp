d6fc883a11580809f364df41c7d23b913ae57c54
Tiago Costa Araujo <37983882+graodopao@users.noreply.github.com>
2024-01-31 18:21:18 -0300

    First window!
    Made following the tutorial at learnopengl.com

--- GraphicsProgramming/main.cpp
+++ GraphicsProgramming/main.cpp
@@ -1,7 +1,65 @@
+// This file was written following the tutorial at learnopengl.com
+
 #include <iostream>
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Callbacks
+void framebuffer_size_callback(GLFWwindow*, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// Misc
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
 
 int main() {
-	std::cout << "Hello, Graphics Programming!";
+	// glfw initialization
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Window creation
+	GLFWwindow* window{ glfwCreateWindow(1280, 720, "Nashira", NULL, NULL) };
+	if (window == NULL) 
+	{
+		std::cout << "Failed to create GLFW window";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// glad initilization
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+	{
+		std::cout << "Failed to initialize GLAD";
+		return -1;
+	}
+	glViewport(0, 0, 1280, 720);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// loop
+	while (!glfwWindowShouldClose(window)) 
+	{
+		processInput(window);
+
+		// rendering commants here
+		glClearColor(40.0f / 255.0f, 28.0f / 255.0f, 51.0f / 255.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Terminate
+	glfwTerminate();
+
 	return 0;
 }
+
+
